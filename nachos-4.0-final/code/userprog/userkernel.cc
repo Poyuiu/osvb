6432c5ba28ff0ea4b2ef5394d201410aaf91eca6
poyu <yoyohihirobert@gmail.com>
2022-05-25 11:09:17 +0800

    de4

--- nachos-4.0-final/code/userprog/userkernel.cc
+++ nachos-4.0-final/code/userprog/userkernel.cc
@@ -197,7 +197,7 @@ void
 UserProgKernel::InitializeAllThreads()
 {
     for (int i = 1; i <= execfileNum; i++){
-        cout << "execfile[" << i << "]: " << execfile[i] << " start " << endl;
+        cerr << "execfile[" << i << "]: " << execfile[i] << " start " << endl;
 		DEBUG(dbgSJF, "yoyoyo");
         int a = InitializeOneThread(execfile[i]);
         cout << "execfile[" << i << "]: " << execfile[i] << " end "<< endl;
